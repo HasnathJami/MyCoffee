47b7b851a5f86840a3ac0d806bd7f027e44c1fdb
hasnath.chowdhury <hasnath.chowdhury@reddotdigitalit.com>
2024-08-25 11:17:26 +0600

    refactoring

--- lib/view/screens/coffee_prefs.dart
+++ lib/view/screens/coffee_prefs.dart
@@ -49,6 +49,8 @@ class _CoffeePrefsState extends State<CoffeePrefs> {
         Row(
           children: [
             const Text('Sugars: '),
+            if (sugars == 0)
+              const Text('No sugars...'),
             for (int i = 0; i < sugars; i++)
               Image.asset(
                 'assets/sugar_cube.png',
